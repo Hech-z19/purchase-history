94f6cab0d29acfb0ae365fcc7128d2fe36858afa
Hech-z19 <78944717+Hech-z19@users.noreply.github.com>
2023-07-06 01:12:09 -0400

    Se agregó el modulo de compras al app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:bSYlGlrTczvW7n4N@purchases.ggkczwg.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    PurchasesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
