2056e269f1a1d228848355c75a501a7af1fc2da0
Hech-z19 <78944717+Hech-z19@users.noreply.github.com>
2023-07-06 01:11:35 -0400

    Configuración del mainJS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Purchases API')
+    .setDescription('The purchases API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(18000);
 }
 bootstrap();
